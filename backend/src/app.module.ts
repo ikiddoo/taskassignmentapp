c12ac0542c44d1f3eec9b48f690c3d956e33aae3
ikiddoo <zulmahmood95@outlook.com>
2025-10-15 08:24:23 +0800

    refactor micro orm config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
@@ -8,7 +9,9 @@ import { DevelopersModule } from './developers/developers.module';
 import { SkillsModule } from './skills/skills.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
-import config from './mikro-orm.config';
+import { Developer } from './developers/entities/developer.entity';
+import { Skill } from './skills/entities/skill.entity';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
@@ -16,7 +19,20 @@ import config from './mikro-orm.config';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    MikroOrmModule.forRoot(config),
+    MikroOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        driver: PostgreSqlDriver,
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        user: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        dbName: configService.get<string>('DB_NAME'),
+        entities: [Developer, Skill, Task],
+        debug: process.env.NODE_ENV !== 'production',
+        allowGlobalContext: true,
+      }),
+    }),
     TasksModule,
     DevelopersModule,
     SkillsModule,
